2315174354a6efd84e56587d7e80a9adabb75c41
gayeon930 <ssgy0930@naver.com>
2025-02-06 21:07:52 +0900

    portfolio update

--- main.js
+++ main.js
@@ -14,3 +14,41 @@ window.addEventListener('resize', function() {
 });
 
 
+let currentPage = 1;
+const totalPages = 3;
+
+const prevBtn = document.querySelector('.slide-navigation .prev');
+const nextBtn = document.querySelector('.slide-navigation .next');
+
+function updateButtons() {
+    if (currentPage === 1) {
+        prevBtn.classList.add('disabled');
+    } else {
+        prevBtn.classList.remove('disabled');
+    }
+
+    // 마지막 페이지일 때 다음 버튼 비활성화
+    if (currentPage === totalPages) {
+        nextBtn.classList.add('disabled');
+    } else {
+        nextBtn.classList.remove('disabled');
+    }
+}
+
+function movePage(direction) {
+    if (direction === 'prev' && currentPage > 1) {
+        currentPage--;
+    } else if (direction === 'next' && currentPage < totalPages) {
+        currentPage++;
+    }
+
+    updateButtons(); 
+}
+
+// 버튼 클릭 시 페이지 이동
+prevBtn.addEventListener('click', () => movePage('prev'));
+nextBtn.addEventListener('click', () => movePage('next'));
+
+// 초기 상태 업데이트
+updateButtons();
+
